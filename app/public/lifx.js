24034fbed30eca19da0e6b7381a53745a635702e
Guy Margalit <guy.margalit0@gmail.com>
2019-03-05 16:39:06 -0800

    Update lifx.js

--- app/public/lifx.js
+++ app/public/lifx.js
@@ -1,5 +1,4 @@
-var LifxClient = require('node-lifx').Client;
-var client = new LifxClient();
+const Lifx = require('./node-lifx-lan');
 
 let devices = [];
 let i = 0;
@@ -7,22 +6,43 @@ let section = 0;
 let effect = 0;
 
 function discover(window) {
-	client.init({
-		resendPacketDelay: 0,
-		resendMaxTimes: 0,
-	});
-	client.on('light-new', device => {
-		devices.push(device);
-		window.webContents.send('lifx-new', device);
-		device.on();
-		device.color(0, 0, 100);
-		console.log(devices);
-	});
+	Lifx.discover()
+		.then(device_list => {
+			if (device_list.length > 0) {
+				return device_list.forEach(device => {
+					devices.push(device);
+					window.webContents.send('lifx-new', device);
+
+					device.turnOn({
+						color: {
+							hue: 0,
+							saturation: 0,
+							brightness: 1.0,
+							kelvin: 3500,
+						},
+						duration: 0.0,
+					});
+				});
+			}
+		})
+		.catch(error => {
+			console.error(error);
+		});
+}
+
+function destroy() {
+	Lifx.destroy()
+		.then(() => {
+			console.log('Bye!');
+		})
+		.catch(error => {
+			console.error();
+		});
 }
 
 function color(info) {
 	if (devices.length > 0) {
-		let device = client.light(devices[i].address);
+		let device = devices[i];
 		if (info.section !== section) {
 			let new_effect = Math.floor(Math.random() * Math.floor(4));
 			section = info.section;
@@ -34,25 +54,62 @@ function color(info) {
 		if (info.color) {
 			switch (effect) {
 				case 0:
-					device.color(info.color.hue * 360, info.color.saturation * 100, info.color.brightness * 100);
+					device.lightSetColor({
+						color: info.color,
+						duration: 0.0,
+					});
 					break;
 				case 1:
 					//FLASH SEQUENTIAL
-					device.color(info.color.hue * 360, info.color.saturation * 100, info.color.brightness * 100);
+					device.lightSetColor({
+						color: {
+							hue: 0.0,
+							saturation: 0.0,
+							brightness: 0.0,
+							kelvin: 3500,
+						},
+						duration: 0.0,
+					});
+					device.lightSetWaveform({
+						transient: 1,
+						color: info.color,
+						period: 100,
+						skew_ratio: 0.5,
+						cycles: 1,
+						waveform: 4,
+					});
 					break;
 				case 2:
 					//ALL LIGHTS
-					client.lights().forEach(light => {
-						light.color(info.color.hue * 360, info.color.saturation * 100, info.color.brightness * 100);
-					});
+					for (let j = 0; j < devices.length; j++) {
+						devices[j].lightSetColor({
+							color: info.color,
+							duration: 0.0,
+						});
+					}
 					break;
 				case 3:
-					client.lights().forEach(light => {
-						light.color(info.color.hue * 360, info.color.saturation * 100, info.color.brightness * 100);
+					device.lightSetColor({
+						color: {
+							hue: 0.0,
+							saturation: 0.0,
+							brightness: 0.0,
+							kelvin: 3500,
+						},
+					});
+					device.lightSetWaveform({
+						transient: 1,
+						color: info.color,
+						period: info.duration,
+						cycles: 1,
+						waveform: 2,
 					});
 					break;
 				default:
-					device.color(info.color.hue * 360, info.color.saturation * 100, info.color.brightness * 100);
+					device.lightSetColor({
+						color: info.color,
+						duration: 0.0,
+					});
 			}
 		}
 
@@ -66,5 +123,6 @@ function color(info) {
 
 module.exports = {
 	discover,
+	destroy,
 	color,
 };
