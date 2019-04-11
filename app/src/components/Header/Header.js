6a0bcdd19d9faa7f0bcff00855a7b66840ae18b0
Guy Margalit <guy.margalit0@gmail.com>
2019-04-10 21:48:05 -0700

    Update Header

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import { MdExitToApp } from 'react-icons/md';
+import { withRouter } from 'react-router';
 import './Header.css';
 
-export default class Header extends Component {
+class Header extends Component {
 	render() {
+		const authToken = localStorage.getItem('AUTH_TOKEN');
 		return (
-			<div className="Header">
-				<div className="Header-keyboard" />
-				<div className="Header-lights" />
+			<div className={authToken ? 'Header' : 'Header-login'}>
+				<div />
+				<div className="Header-block">
+					{authToken ? (
+						<div
+							className="Header-signout"
+							onClick={() => {
+								localStorage.removeItem('AUTH_TOKEN');
+								this.props.history.push(`/login`);
+							}}
+						>
+							<MdExitToApp size={25} />
+						</div>
+					) : null}
+				</div>
 			</div>
 		);
 	}
 }
+
+export default withRouter(Header);
